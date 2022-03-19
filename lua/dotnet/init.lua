0c0bcf339a4aad483b0cda3efffa844529665029
Keng1ne <StyXinator98@googlemail.com>
2022-03-19 21:09:54 +0100

    fixed dotnet

--- lua/dotnet/init.lua
+++ lua/dotnet/init.lua
@@ -1,9 +1,14 @@
-local on_create = function(params)
-  print(params)
+local Path = require('plenary.path')
+local utils = require('telescope._extensions.template_actions.utils')
+
+local on_create = function(method, params)
+  params.cwd = params.projectName
+  utils.create_dir(params.projectName)
+  utils.set_method_args(method, params)
 end
 
-local on_created = function(params)
-  print(params)
+local on_created = function(method, params)
+  print('created')
 end
 
 local create_new_project = {

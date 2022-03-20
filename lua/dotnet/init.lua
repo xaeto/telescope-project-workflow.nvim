4fd7e83ec21991682b66107e1d38d0d44586de43
Keng1ne <StyXinator98@googlemail.com>
2022-03-20 21:44:12 +0100

    added parameter options

--- lua/dotnet/init.lua
+++ lua/dotnet/init.lua
@@ -19,10 +19,47 @@ local on_solution_created = function(method, params)
   print "solution has been created"
 end
 
+local on_add_project_to_solution = function(method, params)
+  print(params.projectSolutionDir)
+  params.cwd = params.projectSolutionDir
+end
+
+local project_added_to_solution = function(method, params)
+end
+
+local add_project_to_solution = {
+  executable = "/usr/bin/dotnet",
+  text = "Add Project to Solution",
+  params = {
+    projectSolutionDir = {
+      type = "file",
+      text = "Solution Directory"
+    },
+    projectDir = {
+      type = "file",
+      text = "Project Directory"
+    }
+  },
+  args = { "sln", "add", "projectDir" },
+  pre = on_add_project_to_solution,
+  post = project_added_to_solution,
+}
+
 local create_new_project = {
   executable = "/usr/bin/dotnet",
   text = "New C# Console Project",
-  params = { "projectDir", "projectName" },
+  params = {
+    projectDir = {
+      type = "file",
+      text = "Project Directory"
+    },
+    projectName = {
+      text = "Project Name",
+      validator = function(value)
+	return true
+      end
+    }
+  },
   args = { "new", "console" },
   pre = on_create_project,
   post = on_project_created,
@@ -31,7 +68,12 @@ local create_new_project = {
 local create_new_solution = {
   executable = "/usr/bin/dotnet",
   text = "New C# Solution",
-  params = { "projectDir" },
+  params = { 
+    projectDir  = {
+      type = "file",
+      text = "Project Directory"
+    }
+  },
   args = { "new", "sln" },
   pre = on_create_solution,
   post = on_solution_created,
@@ -42,5 +84,6 @@ return {
   methods = {
     create_new_project,
     create_new_solution,
+    add_project_to_solution
   },
 }
